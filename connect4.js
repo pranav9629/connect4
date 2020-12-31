62fc91b6202f2dbe852e8e6da359b253d83a90b9
Pranav <46915136+pranav9629@users.noreply.github.com>
2020-12-30 19:10:26 -0500

    Add files via upload

--- connect4.js
+++ connect4.js
@@ -1,1 +1,47 @@
-// console.log("working!");
+console.log("connect4.js is sucessfully linked");
+
+var button = $("button");
+var cells = $("span");
+var header = $("th");
+var clicks = [0,0,0,0,0,0,0];
+var turnChecker = 0;
+
+// button.eq(0).click(function(){
+//   var i = 35-clicks*7;
+//   clicks++;
+//   // console.log(clicks);
+//   if (i%2 === 0) {
+//     cells.eq(i).addClass("redDot");
+//   }else {
+//     cells.eq(i).addClass("blueDot");
+//   }
+// });
+
+// button.eq(0).click(function(){
+//   clicks[0] = clicks[0]+1;
+//   console.log(clicks);
+// });
+
+function boardFill(n) {
+  button.eq(n).click(function(){
+    clicks[n] = clicks[n]+1;
+    var i = (42+n)-(clicks[n]*7);
+    if (turnChecker%2 === 0) {
+      cells.eq(i).addClass("blueDot");
+    }else {
+      cells.eq(i).addClass("redDot");
+    }
+    turnChecker = turnChecker + 1;
+    console.log(turnChecker);
+    console.log(clicks);
+    console.log(i);
+  });
+}
+
+boardFill(0);
+boardFill(1);
+boardFill(2);
+boardFill(3);
+boardFill(4);
+boardFill(5);
+boardFill(6);
